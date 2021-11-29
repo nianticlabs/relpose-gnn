24bc786283c5093b4116717e57de87fb522399c2
Aron Monszpart <aron@nianticlabs.com>
2021-11-29 12:55:52 +0000

    gifs added

--- python/niantic/datasets/graph_structure.py
+++
@@ -1,2 +0,0 @@
-class GraphStructure:
-    FC = 'fc'
--- python/path_config.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-
-PATH_PROJECT = Path(__file__).parent.parent
-PATH_PYTHON = Path(__file__).parent
-
-if 'TORCH_HOME' not in os.environ:
-    os.environ['TORCH_HOME'] = str(PATH_PROJECT / 'models')
